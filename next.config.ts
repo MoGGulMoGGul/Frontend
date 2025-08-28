60f1cdfb88f2945cc7edd38ab5607adc426ed954
GangHyoun <tail6622@naver.com>
2025-08-28 19:16:42 +0900

    Feat : S3 배포 설정

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,14 @@
-import type { NextConfig } from "next";
-
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${API_BASE_URL}/api/:path*`,
-      },
-    ];
-  },
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3001",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "dummyimage.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "moggulmoggul.s3.ap-northeast-2.amazonaws.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'moggulmoggul-frontend.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
